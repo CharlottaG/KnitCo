6418e26c2e18c6e55450e71032f53eef2e0ee287
Charlotta Grundmann <lotta@grundmann.se>
2024-06-10 14:30:52 +0000

    Add database

---
+++ products/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=254, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=254, unique=True)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey('Category', blank=True, null=True, on_delete=models.SET_NULL)
+    subcategory = models.ForeignKey('SubCategory', blank=True, null=True, on_delete=models.SET_NULL)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    ratings = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
