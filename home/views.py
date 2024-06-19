4b95652cc72daccf91317ee736b5474964b10b98
Charlotta Grundmann <lotta@grundmann.se>
2024-06-19 13:02:40 +0000

    Add shopping bag app, templates and urls

--- home/views.py
+++ home/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    "Return to index page"
+    """ Return to index page """
+    
     return render(request, 'home/index.html')
