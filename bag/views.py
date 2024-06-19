b3aa6149557ba3e419f43a1faf7e4b5e3177966e
Charlotta Grundmann <lotta@grundmann.se>
2024-06-19 16:10:32 +0000

    Add functionality to add to bag in views.py

--- bag/views.py
+++ bag/views.py
@@ -6,3 +6,19 @@ def view_bag(request):
     """ Show shopping bag content """
 
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """ Add specified product and quantity to shopping bag """
+    
+    quantity = int(request.POST.get('quantity'))
+    rediredt_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag, {}')
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(rediredt_url)
