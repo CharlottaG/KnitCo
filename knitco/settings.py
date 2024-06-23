754b212c5a77ee0434401306fcdb07c7db7b8fde
Charlotta Grundmann <lotta@grundmann.se>
2024-06-23 18:30:52 +0000

    Add toast messages

--- knitco/settings.py
+++ knitco/settings.py
@@ -115,6 +115,7 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in Django admin, regardless of `allauth`
