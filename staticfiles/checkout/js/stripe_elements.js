d1eb66b14982991f103b115b089df0fe28d71867
Charlotta Grundmann <lotta@grundmann.se>
2024-06-25 16:28:49 +0000

    Add successfull checkout template and view

--- staticfiles/checkout/js/stripe_elements.js
+++ staticfiles/checkout/js/stripe_elements.js
@@ -1,7 +1,4 @@
-const stripe = Stripe(stripePublicKey);
-const stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-const clientSecret = $('#id_client_secret').text().slice(1, -1);
-
+const stripe = Stripe('pk_test_51PVTL2P3kNHn6Hc2EfhOYN5hml14PDsN6vsj2vinkgAXorTAvfl7yp7PsofJFk2Vurvni8PXCxxLQiKFRurVFyqZ00wX0MTg76');
 
 const appearance = {
     theme: 'flat',
@@ -47,12 +44,12 @@ const options = {
     spacedAccordionItems: true
   }
 };
-
+const clientSecret = {{CLIENT_SECRET}};
 const elements = stripe.elements({ clientSecret, appearance });
 const paymentElement = elements.create('payment', options);
 paymentElement.mount('#payment-element');
 
-// Handle realtime validation errors on the payment element
+// Handle realtime validation errors on the card element
 paymentElement.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('payment-errors');
     if (event.error) {
@@ -68,34 +65,3 @@ paymentElement.addEventListener('change', function (event) {
     }
 });
 
-// Handle submit form 
-const form = document.getElementById('payment-form');
-const submitButton = document.getElementById('submit-button');
-const clientSecret = submitButton.dataset.secret;
-
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    paymentElement.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('payment-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            paymentElement.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
-            }
-        }
-    });
-});
