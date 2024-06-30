678614f588c3983847bfa05a85c529ab52f22b13
Charlotta Grundmann <lotta@grundmann.se>
2024-06-30 17:30:34 +0000

    Add display stored info and editing functionality to user profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def profile(request):
-    """ Display the user's profile. """
-
-   # Get logged in user profile
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    context = {
-        'profile': profile
-    }
+    if request.method == 'POST':
+        profile_form = UserProfileForm(request.POST, instance=profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            return redirect('profile')
+    else:
+        profile_form = UserProfileForm(instance=profile)
 
+    context = {
+        'profile_form': profile_form,
+    }
     return render(request, 'profiles/profile.html', context)
 
-    
+@login_required
+def update_profile(request):
+    profile = request.user.userprofile
+
+    if request.method == 'POST':
+        update_profile_form = UserProfileForm(request.POST, instance=profile)
+        if update_profile_form.is_valid():
+            update_profile_form.save()
+            # Update user's email in the User model
+            request.user.email = update_profile_form.cleaned_data['default_email']
+            request.user.save()
+            return redirect('profile')
+    else:
+        update_profile_form = UserProfileForm(instance=profile)
+
+    context = {
+        'update_profile_form': update_profile_form,
+    }
+    return render(request, 'profiles/profile_update.html', context)
+
+
