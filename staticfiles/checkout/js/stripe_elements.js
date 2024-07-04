f241b9799d4919a699a125677ab6343cc4d9b52c
Charlotta Grundmann <lotta@grundmann.se>
2024-07-04 16:12:02 +0000

    Collect static for deploy

--- staticfiles/checkout/js/stripe_elements.js
+++ staticfiles/checkout/js/stripe_elements.js
@@ -1,61 +1,28 @@
-const stripe = Stripe('pk_test_51PVTL2P3kNHn6Hc2EfhOYN5hml14PDsN6vsj2vinkgAXorTAvfl7yp7PsofJFk2Vurvni8PXCxxLQiKFRurVFyqZ00wX0MTg76');
-
-const appearance = {
-    theme: 'flat',
-    variables: {
-        colorPrimary: '#0570de',
-        colorBackground: '#ffffff',
-        colorText: '#30313d',
-        colorDanger: '#df1b41',
-        fontFamily: 'Ideal Sans, system-ui, sans-serif',
-        spacingUnit: '2px',
-        borderRadius: '4px',
-        // See all possible variables below
-      }
-      rules: {
-        '.Tab': {
-          border: '1px solid #E0E6EB',
-          boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02)',
-        },
-  
-        '.Tab:hover': {
-          color: 'var(--colorText)',
-        },
-  
-        '.Tab--selected': {
-          borderColor: '#E0E6EB',
-          boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.03), 0px 3px 6px rgba(18, 42, 66, 0.02), 0 0 0 2px var(--colorPrimary)',
-        },
-  
-        '.Input--invalid': {
-          boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 0 0 2px var(--colorDanger)',
-        },
-  
-        // See all supported class names and selector syntax below
-      }
-    };
-
-const options = {
-  layout: {
-    business: "KnitCo"
-    type: 'accordion',
-    defaultCollapsed: false,
-    radios: false,
-    spacedAccordionItems: true
-  }
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
+var elements = stripe.elements();
+var style = {
+    base: {
+        color: '#000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
 };
-const clientSecret = {{CLIENT_SECRET}};
-const elements = stripe.elements({ clientSecret, appearance });
-const paymentElement = elements.create('payment', options);
-payment.mount('#payment-element');
-
-// Card payment
-var card = elements.create('card', {style: style});
+var card = elements.create('card', { style: style });
 card.mount('#card-element');
 
 // Handle realtime validation errors on the card element
-payment.addEventListener('change', function (event) {
-    var errorDiv = document.getElementById('payment-errors');
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
             <span class="icon" role="alert">
@@ -69,3 +36,70 @@ payment.addEventListener('change', function (event) {
     }
 });
 
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true });
+    $('#submit-button').attr('disabled', true);
+
+    // Capture form data if save-info box is checked
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    // Post data to url after confirm card payment from Stripe
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line1: $.trim(form.street_address.value),
+                        postal_code: $.trim(form.postcode.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address.value),
+                    postal_code: $.trim(form.postcode.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                }
+            }
+        }).then(function (result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                card.update({ 'disabled': false });
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
+            }
+        });
+    // Reload the page if failure, display error without charging user
+    }).fail(function() {
+        location.reload();
+    })
+});
