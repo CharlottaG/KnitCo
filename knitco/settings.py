913c371b07a3b7475a06809672dec82b5b1eff1e
Charlotta Grundmann <lotta@grundmann.se>
2024-07-04 18:40:14 +0000

    Add whitenoise for file storage of  static files

--- knitco/settings.py
+++ knitco/settings.py
@@ -65,7 +65,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-charlottag-knitco-llb449kz7qq.ws.codeinstitute-ide.net', 'knitco-7d640c861cc6.herokuapp.com']
 
@@ -102,7 +102,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware'
+    'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'knitco.urls'
@@ -207,14 +208,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = ''
+#STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'checkout', 'static'),
 ]
 
 # Default primary key field type
